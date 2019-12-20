c8d9f1c8926ad611e469cf10ca957d25dcc33e98
FizzesShark <xbug2001@gmail.com>
2019-12-19 22:25:57 -0800

    Adding sentence finished

---
+++ markov.js
@@ -0,0 +1,41 @@
+const seps = new Set(['.', '!', '?']);
+class MarkovChain {
+	constructor(n) {
+		//	The n in n-gram
+		this.n = n;
+
+		//	The array storing all sentence beginnings
+		this.start = [];
+
+		//	The dictionary matching n-grams to possible values
+		this.Chain = {};
+	}
+
+	//	Adding a single sentence to the chain
+	trainSentence(string) {
+		const words = string.split(' ').filter(s => s !== ' ').filter(s => s !== '');
+
+		//	The sub-section of the sentence we'll be looking at
+		const buffer = []
+
+		if (words.length <= this.n) {
+			return;
+		}
+
+		this.start.push(words.slice(0, this.n + 1));
+		buffer.push(words.slice(0, this.n + 1));
+		words.splice(0, this.n);
+
+		for (const w of words) {
+			buffer.push(w);
+			const key = buffer.slice(0, this.n);
+			if (this.Chain.has(key)) {
+				this.Chain[key].push(buffer[buffer.length - 1]);
+			} else {
+				this.Chain[key] = buffer[buffer.length - 1];
+			}
+
+			buffer.slice(1, buffer.length - 1);
+		}
+	}
+}
